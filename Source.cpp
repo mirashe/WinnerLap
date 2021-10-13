b6db0e534b531ad6323448a85ce941fc5db02b7b
Nabi Mirashe <nabi.mirashe@uniface.com>
2021-10-13 20:19:13 +0200

    refactored parsing the clock time & finding best lap

--- Source.cpp
+++ Source.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <chrono>
 #include <time.h>
+#include <tuple>
 
 using recordClock = std::chrono::system_clock::time_point;
 
@@ -32,6 +33,8 @@ public:
         mRecordsStream.close();
     }
 
+    // returns true in case of successful read, returns false in case of finishing the string.
+    // throws domain exception when parse operation failes
     bool getNextRecord(unsigned int& currentKartNumber, recordClock& parsedClock) {
         std::string kartNumberString{};
         std::getline(mRecordsStream, kartNumberString, ',');
@@ -42,42 +45,41 @@ public:
         currentKartNumber = std::stoul(kartNumberString);
 
         std::string currentClockString;
-        std::getline(mRecordsStream, currentClockString);
-        std::stringstream timeStringStream(currentClockString);
-
-        std::time_t t = std::time(0);   // get time now
-        std::tm now;
-        localtime_s(&now, &t);
-
-        std::tm parsedTimeObject{};
-        timeStringStream >> std::get_time(&now, "%H:%M:%S");
-        if (timeStringStream.fail())
-        {
-            std::cout << "parse failed";
-            throw std::domain_error("Can not parse the given timestamp: " + currentClockString);
-        }
-
-        auto tt = std::mktime(&now);
-        parsedClock = std::chrono::system_clock::from_time_t(tt);
+        std::getline(mRecordsStream, currentClockString);        
+        parsedClock = parseClock(currentClockString);
     }
 
 private:
+    recordClock parseClock(const std::string& clockTime) const {
+        std::stringstream timeStringStream(clockTime);
+        std::time_t t = std::time(0);   // get time now
+        std::tm parsedTimeObject;
+        localtime_s(&parsedTimeObject, &t);
+        timeStringStream >> std::get_time(&parsedTimeObject, "%H:%M:%S");
+
+        if (timeStringStream.fail())
+        {
+            std::cerr << "parse failed";
+            throw std::domain_error("Can not parse the given timestamp: " + clockTime);
+        }
+
+        auto tt = std::mktime(&parsedTimeObject);
+        recordClock result = std::chrono::system_clock::from_time_t(tt);
+
+        return result;
+    }
+
     std::ifstream mRecordsStream;
 };
 
-int main() {
-    recordsReader reader;
-    
-    if (!reader.open("../karttimes.csv"))
-        return 1;
+auto getBestLap(recordsReader& reader)
+{
+    unsigned int bestLapKartNumber{};
+    auto bestLapTime = std::chrono::system_clock::duration::max();
 
     // to keep it simple I assumed at the begining we always have all karts records ordered,
     // otherwise we can use a map instead of vector
     std::vector<recordClock> lapTimes;
-
-    int bestLapKartNumber{};
-    auto bestLapTime = std::chrono::system_clock::duration::max();
-
     unsigned int kartNumber{};
     recordClock parsedClock{};
 
@@ -96,9 +98,26 @@ int main() {
             lapTimes[kartNumber - 1] = parsedClock;
         }
     }
+
+    return std::tuple{ bestLapKartNumber, bestLapTime };
+}
+
+int main() {
+    recordsReader reader;
     
-    std::cout << "Best lap seconds: " << std::chrono::duration_cast<std::chrono::seconds>(bestLapTime).count() << std::endl;
-    std::cout << "Best lap kart number : " << bestLapKartNumber << std::endl;
+    if (!reader.open("../karttimes.csv"))
+        return 1;
+
+    try {
+        auto [bestLapKartNumber, bestLapTime] = getBestLap(reader);
+
+        std::cout << "Best lap seconds: " << std::chrono::duration_cast<std::chrono::seconds>(bestLapTime).count() << std::endl;
+        std::cout << "Best lap kart number : " << bestLapKartNumber << std::endl;
+    }
+    catch (std::exception ex) {
+        std::cerr << "Error: " << ex.what() << std::endl;
+        return 1;
+    }
 
     return 0;
 }
